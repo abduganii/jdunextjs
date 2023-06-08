d2ca558733c21c18e7e70143d79ee35bd2f9b1ff
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-06-08 07:27:25 +0500

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 'use client'
-import { Suspense, useEffect } from 'react';
-import NextNProgress from 'nextjs-progressbar';
+// import { Suspense, useEffect } from 'react';
+// import NextNProgress from 'nextjs-progressbar';
 import '../styles/globals.scss'
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 
 function MyApp({ Component, pageProps }) {
@@ -13,11 +14,14 @@ function MyApp({ Component, pageProps }) {
     } 
   },[])
   return (
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <NextNProgress color="#29D" startPosition={0.1} stopDelayMs={100} height={2} showOnShallow={false}/>
-          {/* <Component {...pageProps} /> */}
+      // <Suspense fallback={<h1>Loading...</h1>}>
+      //   <NextNProgress color="#29D" startPosition={0.1} stopDelayMs={100} height={2} showOnShallow={false}/>
+      //     <Component {...pageProps} />
       
-      </Suspense>
+      // </Suspense>
+    <>
+    home
+    </>
   )
 }
 
